1125a18573ccf99539ba60f92dfcf1cbdb00f45d
Ryan McCahon <mccahonr@yahoo.com>
2019-08-16 16:37:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+require "pry"
+
+def prime?(integer)
+  if integer <= 1
+    false
+  elsif integer <= 3
+    true
+  else (2..n/2).none? do |i|
+    integer % i == 0    
+  end  
+end
