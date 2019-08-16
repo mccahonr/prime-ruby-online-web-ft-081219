25895a8de26d72f9be2791cbaab1c6fee795042d
Ryan McCahon <mccahonr@yahoo.com>
2019-08-16 16:39:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+require "pry"
+
+def prime?(integer)
+  if integer <= 1
+    false
+  elsif integer <= 3
+    true
+  else (2..integer/2).none? do |i|
+    integer % i == 0
+    end
+  end
+end
